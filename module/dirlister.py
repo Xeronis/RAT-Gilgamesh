b6c5d6c2f06465394b85cfa8a5040b71c8974035
Xeronis <johannesteban280@gmail.com>
2025-12-15 04:29:47 -0500

    Renombrando carpeta módulos a modules

--- module/dirlister.py
+++ module/dirlister.py
@@ -1,8 +1,8 @@
 import os 
 def run(**args):
-print("[*] In dirlister module")
-files = os.listdir(".")
-return str(files)
+               print("[*] In dirlister module")
+               files = os.listdir(".")
+               return str(files)
 
 
 
